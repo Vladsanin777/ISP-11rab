25ce31c1176f75b2d59ffe4d6584bf686e29808c
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2024-05-09 23:05:19 +0400

    Игра Орёл и Решка

--- cogs/game.py
+++ cogs/game.py
@@ -1,6 +1,7 @@
 import disnake
 from disnake.ext import commands
 from disnake import Colour
+import random
 
 
 class Game(commands.Cog):
@@ -14,9 +15,15 @@ class Game(commands.Cog):
 
     @commands.slash_command(name = "монетка", description = "Игра в Орёл и Решку")
     async def coin(self, ctx, coin: str = commands.Param(name = "сторона", description = "Выбери сторону монеты", choices = ["Орёл", "Решка"]), ui: str = commands.Param(default="Только я", name="видимость", choices=["Только я", "Все участники чата"])):
-        print(random(2))
-
-
+        if (rand := bool(random.randint(0,1))) and coin == "Орёл" or (not rand) and coin == "Решка":
+            result = "**Ты угодал!!!**"
+            colour = 0x33FF18
+            computer = coin
+        else:
+            result = "**Ты не угодал!!!**"
+            colour = 0xFF1B18
+            computer = ["Орёл", "Решка"][int(not bool(["Орёл", "Решка"].index(coin)))]
+        await ctx.send(embed = disnake.Embed(description = result, colour = colour).add_field(name = "Ты загодал:", value = coin, inline = False).add_field(name = "Компьютер загодал:", value = computer), ephemeral=True if ui == "Только я" else False)
 
 
 def setup(bot):
